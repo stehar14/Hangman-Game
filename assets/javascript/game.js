375bcfc37ec4b2cd24a4f12a1018b23d9f9f7767
Steve Harold <stehar14@yahoo.com>
2017-11-18 01:34:03 -0500

    set sound durations, deleted console logs, editing comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 /*Hangman Game Homework #3
 	Created 11/12/17
-	Last updated 11/16/17
+	Last updated 11/17/17
 	Written by: Steve Harold*/
 
 //array containing possible words and possible guesses	
-var possibleWords = ["falcons", "giants", "cowboys", "redskins", "eagles", "redskins", "jets", "patriots", "bills", "dolphins", "touchdown", "safety", "field goal", "tackle", "wide receiver", "tight end", "quarterback", "running back", "coach"];
+var possibleWords = ["falcons", "giants", "cowboys", "redskins", "eagles", "redskins", "jets", "patriots", "bills", "dolphins", "touchdown", "safety", "goal", "tackle", "receiver", "quarterback", "tennis", "coach", "basketball", "baseball", "football", "volleyball", "basket"];
 possibleGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//set wins and losses to 0	
+
+//set wins and losses to 0, guesses to 8, and define a variable linked to the #stickman canvas element
 var wins = 0;
 var losses = 0;
-var guesses = 8;
+var guesses = 9;
 var canvas = document.getElementById('stickman');
 
 //assign sounds
@@ -34,31 +35,31 @@ function drawLine(context, from, to) {
 
 function drawCanvas() {
 	var c = canvas.getContext('2d');
+
 // reset the canvas and set basic styles
 	canvas.width = canvas.width;
 	c.lineWidth = 10;
 	c.strokeStyle = 'green';
 	c.font = 'bold 24px Optimer, Arial, Helvetica, sans-serif';
 	c.fillStyle = 'red';
-// draw the ground
-	drawLine(c, [20,190], [180,190]);
+
 // start building the gallows if there's been a bad guess
 	if (guesses <= 8) {
 // create the upright
 		c.strokeStyle = '#A52A2A';
 		drawLine(c, [30,185], [30,10]);
 		if (guesses <= 7) {
-    // create the arm of the gallows
+// create the arm of the gallows
     		c.lineTo(150,10);
     		c.stroke();
 		}
 		if (guesses <= 6) {
     		c.strokeStyle = 'black';
     		c.lineWidth = 3;
-    // draw rope
+// draw rope
     		drawLine(c, [145,15], [145,30]);
     	}
-    // draw head
+// draw head
     	if (guesses <= 5) {
     		c.beginPath();
    			c.moveTo(160, 45);
@@ -66,23 +67,23 @@ function drawCanvas() {
     		c.stroke(); 
 		}
 		if (guesses <= 4) {
-    // draw body
+// draw body
     		drawLine(c, [145,60], [145,130]);
 		}
 		if (guesses <= 3) {
-    // draw left arm
+// draw left arm
     		drawLine(c, [145,80], [110,90]);
 		}
 		if (guesses <= 2) {
-    // draw right arm
+// draw right arm
     		drawLine(c, [145,80], [180,90]);
 		}
 		if (guesses <= 1) {
-    // draw left leg
+// draw left leg
     		drawLine(c, [145,130], [130,170]);
 		}
 		if (guesses <= 0) {
-    // draw right leg and end game
+// draw right leg and end game
     		drawLine(c, [145,130], [160,170]);
     		c.fillText('Game over', 45, 110);
 		}
@@ -98,14 +99,10 @@ function game(){
 	var word = possibleWords[Math.floor(Math.random()*possibleWords.length)];
 	var wordArray = word.split("");
 
-/*prints word and word array to console (testing)
-	console.log(word);	
-	console.log(wordArray);*/
-
 //declare blanks to keep track of letters not yet guessed and guesses to track remaining guesses	
 	var blanks = word.length;
 	drawCanvas();
-	document.getElementById("guesses").innerHTML = 8;
+	document.getElementById("guesses").innerHTML = 9;
 		
 //declares empty arrays to hold answer and guessed letter
 	var answer = [];
@@ -115,35 +112,30 @@ function game(){
 	for ( i = 0; i < word.length; i++){
 		answer[i] = "_";
 	}
+
 //shows answer array on webpage
 	document.getElementById("answer").innerHTML = answer.join(" ");
 
-/*prints answer array and blanks to console (testing)
-	console.log(answer);
-	console.log(blanks);*/
-	
 //user choices taken from keyboard input
     document.onkeyup = function(event) {
     	
 //if there are still letters to fill in AND guesses remaining, run the code
     	if ((blanks > 0) && (guesses > 0)) {
 
-//declares variable to convert users guesses to lower case and store them
 			var key = event.key.toLowerCase();
-
-//checking to see if word contains user guess and the guessed letter hasn't been guessed yet
 			if (word.includes(key) && (guessedLetters.includes(key.toUpperCase()) === false)){
 				
-//check user guess against each character in the word
+//check user guess against each character in the word, if it matches, add to corresponding index of answer array
 				for (k = 0; k < word.length; k++){
 					
-//if user input matches a character in the word, replace the corresbonding blank in the answer array
 					if (key === wordArray[k]) {
 					answer[k] = key.toUpperCase();
 
 //update HTML with answer, subtract one blank for each time the key matches
 					document.getElementById("answer").innerHTML = answer.join(" ");
 					blanks--;
+
+//play soung for correct answer
 					setTimeout(function(){
 					    right.play();
 
@@ -186,7 +178,7 @@ function game(){
 				alert("You already chose that letter!");
 			}
 
-//if the letter doesn't match and hasn;t been guessed already, it is wrong
+//if the letter doesn't match and hasn't been guessed already, it is wrong
 //subtracts a guess and updates html with new guesses left and guessed letter
 			else {
 				guesses--;
@@ -203,10 +195,6 @@ function game(){
 				document.getElementById("guessedLetters").innerHTML = guessedLetters.join(" ");
 				drawCanvas();
 			}
-
-			/*testing
-			console.log(answer);
-			console.log("Guesses:" + guesses);*/
 		}
 
 //if the user guesses all letters in the word, they win, guessed letters array resets
@@ -220,11 +208,11 @@ function game(){
 				    setTimeout(function(){
 				        win.pause();
 				        win.currentTime = 0;
-				    }, 20000);
+				    }, 2000);
 				}, 0);
 				document.getElementById("wins").innerHTML = wins;
 				alert("You win!!!");
-				guesses = 8;
+				guesses = 9;
 				guessedLetters = ["-"];
 				document.getElementById("guessedLetters").innerHTML = guessedLetters.join(" ");
 				game();
@@ -246,7 +234,7 @@ function game(){
 				}, 0);
 				document.getElementById("losses").innerHTML = losses;
 				alert("You lose!");
-				guesses = 8;
+				guesses = 9;
 				guessedLetters= ["-"];
 				document.getElementById("guessedLetters").innerHTML = guessedLetters.join(" ");
 				game();
